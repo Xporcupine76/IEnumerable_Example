7a87e987d353c7a18fac28c29f7f014be10bcce8
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:53:06 +0200

    Add project files.

---
+++ IEnumerable_Example/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+namespace IEnumerable_Example
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<string> cars = new List<string>();
+            cars.Add("Chevrolet");
+            cars.Add("Honda");
+            cars.Add("Lexus");
+            cars.Add("Ford");
+            cars.Remove(cars[3]);
+
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine("\n now our own collection... "+ "\n");
+
+            Car c = new Car();
+            c[0] = "Chevrolet";
+            c[1] = "Mercedes";
+            c[2] = "Dodge";
+
+            foreach (string car in c)
+            {
+                Console.WriteLine(car);
+            }
+
+        }
+
+
+    }
+
+
+}
+public class Car : IEnumerable
+{
+
+    string[] car = new string[3];
+    public string this[int carNum]
+    {
+
+        get { return car[carNum]; }
+        set { car[carNum] = value; }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (string c in car)
+        {
+            yield return c;
+        }
+    }
+}
